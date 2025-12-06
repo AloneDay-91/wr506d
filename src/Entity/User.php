039c51eada80136007fd51b41e2c1d6329ef9320
Elouan Bruzek <elelouan@gmail.com>
2025-12-06 15:35:08 +0100

    feat(user): add password hashing via State Processor and restrict access to admin

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,75 +2,112 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Put;
 use App\Repository\UserRepository;
+use App\State\UserPasswordHasher;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => ['user:read']],
+    denormalizationContext: ['groups' => ['user:create', 'user:update']],
+    graphQlOperations: [
+        new \ApiPlatform\Metadata\GraphQl\Query(security: "is_granted('ROLE_ADMIN')"),
+        new \ApiPlatform\Metadata\GraphQl\QueryCollection(security: "is_granted('ROLE_ADMIN')"),
+        new \ApiPlatform\Metadata\GraphQl\Mutation(
+            name: 'create',
+            denormalizationContext: ['groups' => ['user:create']],
+            validationContext: ['groups' => ['Default', 'user:create']],
+            processor: UserPasswordHasher::class
+        ),
+        new \ApiPlatform\Metadata\GraphQl\Mutation(
+            name: 'update',
+            denormalizationContext: ['groups' => ['user:update']],
+            security: "is_granted('ROLE_USER') or is_granted('ROLE_ADMIN')",
+            processor: UserPasswordHasher::class
+        ),
+        new \ApiPlatform\Metadata\GraphQl\DeleteMutation(name: 'delete', security: "is_granted('ROLE_ADMIN')"),
+    ]
+)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[GetCollection]
-#[Post(security: "is_granted('PUBLIC_ACCESS')")]
+#[GetCollection(security: "is_granted('ROLE_ADMIN')")]
+#[Post(processor: UserPasswordHasher::class, validationContext: ['groups' => ['Default', 'user:create']])]
 #[Get(security: "is_granted('ROLE_ADMIN')")]
-#[Put(security: "is_granted('ROLE_USER') or is_granted('ROLE_ADMIN')")]
-#[Patch(security: "is_granted('ROLE_USER') or is_granted('ROLE_ADMIN')")]
+#[Put(security: "is_granted('ROLE_USER') or is_granted('ROLE_ADMIN')", processor: UserPasswordHasher::class)]
+#[Patch(security: "is_granted('ROLE_USER') or is_granted('ROLE_ADMIN')", processor: UserPasswordHasher::class)]
+#[Delete(security: "is_granted('ROLE_ADMIN')")]
 
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['user:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
     #[Assert\NotBlank]
     #[Assert\Email(message: 'The email {{ value }} is not a valid email.')]
+    #[Groups(['user:read', 'user:create', 'user:update'])]
     private ?string $email = null;
 
     /**
      * @var list<string> The user roles
      */
     #[ORM\Column]
+    #[Groups(['user:read', 'user:create', 'user:update'])]
     private array $roles = [];
 
     /**
      * @var string The hashed password
      */
     #[ORM\Column]
-    #[Assert\NotBlank]
     private ?string $password = null;
+
+    #[Assert\NotBlank(groups: ['user:create'])]
+    #[Groups(['user:create', 'user:update'])]
+    #[ApiProperty(readable: false, writable: true)]
+    private ?string $plainPassword = null;
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank]
     #[Assert\Type(type: 'string', message: 'The firstname must be a string.')]
+    #[Groups(['user:read', 'user:create', 'user:update'])]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank]
     #[Assert\Type(type: 'string', message: 'The lastname must be a string.')]
+    #[Groups(['user:read', 'user:create', 'user:update'])]
     private ?string $lastname = null;
 
     #[ORM\Column(type: 'integer', options: ['default' => 100])]
     #[Assert\Positive(message: 'The rate limit must be a positive number.')]
+    #[Groups(['user:read'])]
     private int $rateLimit = 100;
 
     #[ORM\ManyToOne(targetEntity: MediaObject::class, inversedBy: 'users')]
+    #[Groups(['user:read', 'user:create', 'user:update'])]
     private ?MediaObject $photo = null;
 
     /**
      * @var Collection<int, Review>
      */
     #[ORM\OneToMany(targetEntity: Review::class, mappedBy: 'user', orphanRemoval: true)]
+    #[Groups(['user:read'])]
     private Collection $reviews;
 
     public function __construct()
@@ -138,6 +175,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPassword(string $password): static
     {
         $this->password = $password;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(?string $plainPassword): static
+    {
+        $this->plainPassword = $plainPassword;
 
         return $this;
     }
